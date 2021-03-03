112cfc0294a8ffe50eedd253918e8b6d7e3fe4d5
unknown <mdabuyousufsajib@gmail.com>
2021-03-03 10:51:26 +0600

    javascript practice

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+
+//if condition//
+/*
+var result = 10;
+
+
+if( result >=80 &&   result <=100){
+
+    document.write('Congratulations You Have Pass The Exam:) Your Result is:A+ and Your Number Is:'+result);
+
+}
+else if(  result >=70 &&  result <=79 ){
+    document.write('Congratulations You Have Pass The Exam:) Your Result is:A and Your Number Is:'+result);
+}
+
+else if( result>=69 &&  result<=70 ){
+    document.write('Congratulations You Have Pass The Exam:) Your Result is:A- and Your Number Is:'+result);
+}
+else if( result>=59 &&  result<=60 ){
+    document.write('Congratulations You Have Pass The Exam:) Your Result is:B and Your Number Is:'+result);
+}
+else if( result>=49 &&  result<=50 ){
+    document.write('Congratulations You Have Pass The Exam:) Your Result is:C and Your Number Is:'+result);
+}
+else if( result>=39 &&  result<=40 ){
+    document.write('Congratulations You Have Pass The Exam:) Your Result is:D and Your Number Is:'+result);
+}
+
+
+else{
+    document.write('you fail in the exam good luck next time'+'you number is:'+ result);
+}
+
+
+*/
+
+
+
+
+// switch  statement///
+
+
+
+
+
+
+var latter ='z';
+   
+
+
+switch(latter)
+	{
+		
+		
+		case "a": document.write(latter + " is a vowel.");
+			break;
+		case "e": document.write(latter+ " is a vowel.");
+			break;
+		case "i": document.write(latter+ " is a vowel.");
+			break;
+		case "o": document.write(latter + " is a vowel.");
+			break;
+		case "u": document.write(latter + " is a vowel.");
+			break;
+
+            default: document.write(latter+ " is a consonant.");
+	}
